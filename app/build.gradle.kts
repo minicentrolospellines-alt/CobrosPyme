34e906594ebd538c1a20edd1c94306586972c209
minicentrolospellines-alt <minicentrolospellines@gmail.com>
2026-09-08 22:40:17 -0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,9 +26,9 @@ android {
 }
 
 dependencies {
-    implementation("androidx.activity:activity-compose:1.13.0")
+    implementation("androidx.activity:activity-compose:1.11.0")
 
-    val composeBom = platform("androidx.compose:compose-bom:2026.08.00")
+val composeBom = platform("androidx.compose:compose-bom:2026.04.01")
     implementation(composeBom)
 
     implementation("androidx.compose.ui:ui")
