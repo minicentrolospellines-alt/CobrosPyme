1befaa2ebd8146fdd6b88ab4d9b5cf591492194d
minicentrolospellines-alt <minicentrolospellines@gmail.com>
2026-09-11 01:31:26 -0300

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "cl.negociospyme.cobros"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.3"
+        versionCode = 3
+        versionName = "1.4"
     }
 
     buildFeatures {
@@ -38,6 +38,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.foundation:foundation")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material:material-icons-extended")
 
     debugImplementation("androidx.compose.ui:ui-tooling")
 }
